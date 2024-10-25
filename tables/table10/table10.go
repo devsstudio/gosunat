0a631050e78aedf0a7980edf5e77dfa50f07b190
jean.perea <jean.perea@rappi.com>
2024-10-24 22:22:04 -0500

    Se agrega tabla 10 de documentos

---
+++ tables/table10/table10.go
@@ -0,0 +1,145 @@
+package table10
+
+type Table10 string
+
+const (
+	DOC_00 Table10 = "00"
+	DOC_01 Table10 = "01"
+	DOC_02 Table10 = "02"
+	DOC_03 Table10 = "03"
+	DOC_04 Table10 = "04"
+	DOC_05 Table10 = "05"
+	DOC_06 Table10 = "06"
+	DOC_07 Table10 = "07"
+	DOC_08 Table10 = "08"
+	DOC_09 Table10 = "09"
+	DOC_10 Table10 = "10"
+	DOC_11 Table10 = "11"
+	DOC_12 Table10 = "12"
+	DOC_13 Table10 = "13"
+	DOC_14 Table10 = "14"
+	DOC_15 Table10 = "15"
+	DOC_16 Table10 = "16"
+	DOC_17 Table10 = "17"
+	DOC_18 Table10 = "18"
+	DOC_19 Table10 = "19"
+	DOC_20 Table10 = "20"
+	DOC_21 Table10 = "21"
+	DOC_22 Table10 = "22"
+	DOC_23 Table10 = "23"
+	DOC_24 Table10 = "24"
+	DOC_25 Table10 = "25"
+	DOC_26 Table10 = "26"
+	DOC_27 Table10 = "27"
+	DOC_28 Table10 = "28"
+	DOC_29 Table10 = "29"
+	DOC_30 Table10 = "30"
+	DOC_31 Table10 = "31"
+	DOC_32 Table10 = "32"
+	DOC_33 Table10 = "33"
+	DOC_34 Table10 = "34"
+	DOC_35 Table10 = "35"
+	DOC_36 Table10 = "36"
+	DOC_37 Table10 = "37"
+	DOC_40 Table10 = "40"
+	DOC_41 Table10 = "41"
+	DOC_42 Table10 = "42"
+	DOC_43 Table10 = "43"
+	DOC_44 Table10 = "44"
+	DOC_45 Table10 = "45"
+	DOC_46 Table10 = "46"
+	DOC_48 Table10 = "48"
+	DOC_49 Table10 = "49"
+	DOC_50 Table10 = "50"
+	DOC_51 Table10 = "51"
+	DOC_52 Table10 = "52"
+	DOC_53 Table10 = "53"
+	DOC_54 Table10 = "54"
+	DOC_55 Table10 = "55"
+	DOC_56 Table10 = "56"
+	DOC_87 Table10 = "87"
+	DOC_88 Table10 = "88"
+	DOC_89 Table10 = "89"
+	DOC_91 Table10 = "91"
+	DOC_96 Table10 = "96"
+	DOC_97 Table10 = "97"
+	DOC_98 Table10 = "98"
+)
+
+var TABLE_10_ITEMS = map[Table10]string{
+	DOC_00: "Otros",
+	DOC_01: "Factura",
+	DOC_02: "Recibo por Honorarios",
+	DOC_03: "Boleta de Venta",
+	DOC_04: "Liquidación de compra",
+	DOC_05: "Boletos de Transporte Aéreo que emiten las Compañías de Aviación Comercial por el servicio de transporte aéreo regular de pasajeros, emitido de manera manual, mecanizada o por medios electrónicos (BME)",
+	DOC_06: "Carta de porte aéreo por el servicio de transporte de carga aérea",
+	DOC_07: "Nota de crédito",
+	DOC_08: "Nota de débito",
+	DOC_09: "Guía de remisión - Remitente",
+	DOC_10: "Recibo por Arrendamiento",
+	DOC_11: "Póliza emitida por las Bolsas de Valores, Bolsas de Productos o Agentes de Intermediación por operaciones realizadas en las Bolsas de Valores o Productos o fuera de las mismas, autorizadas por SMV",
+	DOC_12: "Ticket o cinta emitido por máquina registradora",
+	DOC_13: "Documentos emitidos por las empresas del sistema financiero y de seguros, y por las cooperativas de ahorro y crédito no autorizadas a captar recursos del público, que se encuentren bajo el control de la Superintendencia de Banca, Seguros y AFP.",
+	DOC_14: "Recibo por servicios públicos de suministro de energía eléctrica, agua, teléfono, telex y telegráficos y otros servicios complementarios que se incluyan en el recibo de servicio público",
+	DOC_15: "Boletos emitidos por el servicio de transporte terrestre regular urbano de pasajeros y el ferroviario público de pasajeros prestado en vía férrea local.",
+	DOC_16: "Boletos de viaje emitidos por las empresas de transporte nacional de pasajeros, siempre que cuenten con la autorización de la autoridad competente, en las rutas autorizadas. Vía terrestre o ferroviario público no emitido por medios electrónicos (BVME)",
+	DOC_17: "Documento emitido por la Iglesia Católica por el arrendamiento de bienes inmuebles",
+	DOC_18: "Documento emitido por las Administradoras Privadas de Fondo de Pensiones que se encuentran bajo la supervisión de la Superintendencia de Banca, Seguros y AFP",
+	DOC_19: "Boleto o entrada por atracciones y espectáculos públicos",
+	DOC_20: "Comprobante de Retención",
+	DOC_21: "Conocimiento de embarque por el servicio de transporte de carga marítima",
+	DOC_22: "Comprobante por Operaciones No Habituales",
+	DOC_23: "Pólizas de Adjudicación emitidas con ocasión del remate o adjudicación de bienes por venta forzada, por los martilleros o las entidades que rematen o subasten bienes por cuenta de terceros",
+	DOC_24: "Certificado de pago de regalías emitidas por PERUPETRO S.A",
+	DOC_25: "Documento de Atribución (Ley del Impuesto General a las Ventas e Impuesto Selectivo al Consumo, Art. 19º, último párrafo, R.S. N° 022-98-SUNAT).",
+	DOC_26: "Recibo por el Pago de la Tarifa por Uso de Agua Superficial con fines agrarios y por el pago de la Cuota para la ejecución de una determinada obra o actividad acordada por la Asamblea General de la Comisión de Regantes o Resolución expedida por el Jefe de la Unidad de Aguas y de Riego (Decreto Supremo N° 003-90-AG, Arts. 28 y 48)",
+	DOC_27: "Seguro Complementario de Trabajo de Riesgo",
+	DOC_28: "Documentos emitidos por los servicios aeroportuarios prestados a favor de los pasajeros, mediante mecanismo de etiquetas autoadhesivas.  ",
+	DOC_29: "Documentos emitidos por la COFOPRI en calidad de oferta de venta de terrenos, los correspondientes a las subastas públicas y a la retribución de los servicios que presta",
+	DOC_30: "Documentos emitidos por las empresas que desempeñan el rol adquirente en los sistemas de pago mediante tarjetas de crédito y débito, emitidas por bancos e instituciones financieras o crediticias, domiciliados o no en el país.",
+	DOC_31: "Guía de Remisión - Transportista",
+	DOC_32: "Documentos emitidos por las empresas recaudadoras de la denominada Garantía de Red Principal a la que hace referencia el numeral 7.6 del artículo 7° de la Ley N° 27133 – Ley de Promoción del Desarrollo de la Industria del Gas Natural",
+	DOC_33: "Manifiesto de Pasajeros",
+	DOC_34: "Documento del Operador",
+	DOC_35: "Documento del Partícipe",
+	DOC_36: "Recibo de Distribución de Gas Natural",
+	DOC_37: "Documentos que emitan los concesionarios del servicio de revisiones técnicas vehiculares, por la prestación de dicho servicio",
+	DOC_40: "Comprobante de Percepción",
+	DOC_41: "Comprobante de Percepción - Venta interna",
+	DOC_42: "Documentos emitidos por las empresas que desempeñan el rol adquiriente en los sistemas de pago mediante tarjetas de crédito emitidas por ellas mismas",
+	DOC_43: "Boletos emitidos por las Compañías de Aviación Comercial que prestan servicios de transporte aéreo no regular de pasajeros y transporte aéreo especial de pasajeros.",
+	DOC_44: "Billetes de lotería, rifas y apuestas. ",
+	DOC_45: "Documentos emitidos por centros educativos y culturales, universidades, asociaciones y fundaciones, en lo referente a actividades no gravadas con tributos administrados por la SUNAT.",
+	DOC_46: "Formulario de Declaración - pago o Boleta de pago de tributos Internos",
+	DOC_48: "Comprobante de Operaciones - Ley N° 29972",
+	DOC_49: "Constancia de Depósito - IVAP (Ley 28211)",
+	DOC_50: "Declaración Única de Aduanas - Importación definitiva",
+	DOC_51: "Póliza o DUI Fraccionada",
+	DOC_52: "Despacho Simplificado - Importación Simplificada",
+	DOC_53: "Declaración de Mensajería o Courier",
+	DOC_54: "Liquidación de Cobranza",
+	DOC_55: "BVME para transporte ferroviario de pasajeros",
+	DOC_56: "Comprobante de pago SEAE",
+	DOC_87: "Nota de Crédito Especial",
+	DOC_88: "Nota de Débito Especial",
+	DOC_89: "Nota de Ajuste de Operaciones - Ley N° 29972",
+	DOC_91: "Comprobante de No Domiciliado",
+	DOC_96: "Exceso de crédito fiscal por retiro de bienes",
+	DOC_97: "Nota de Crédito - No Domiciliado",
+	DOC_98: "Nota de Débito - No Domiciliado",
+}
+
+func ValidateCode(code string) bool {
+	allowedCodes := []Table10{DOC_00, DOC_01, DOC_02, DOC_03, DOC_04, DOC_05, DOC_06, DOC_07, DOC_08, DOC_09, DOC_10, DOC_11, DOC_12, DOC_13, DOC_14, DOC_15, DOC_16, DOC_17, DOC_18, DOC_19, DOC_20, DOC_21, DOC_22, DOC_23, DOC_24, DOC_25, DOC_26, DOC_27, DOC_28, DOC_29, DOC_30, DOC_31, DOC_32, DOC_33, DOC_34, DOC_35, DOC_36, DOC_37, DOC_40, DOC_41, DOC_42, DOC_43, DOC_44, DOC_45, DOC_46, DOC_48, DOC_49, DOC_50, DOC_51, DOC_52, DOC_53, DOC_54, DOC_55, DOC_56, DOC_87, DOC_88, DOC_89, DOC_91, DOC_96, DOC_97, DOC_98}
+	for _, validCode := range allowedCodes {
+		if Table10(code) == validCode {
+			return true
+		}
+	}
+	return false
+}
+
+func GetName(value string) string {
+	return TABLE_10_ITEMS[Table10(value)]
+}
